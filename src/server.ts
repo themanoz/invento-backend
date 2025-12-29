32e937cb16f214854fad40f975e82aa1a464b796
Manoj <manojkumarbonala3@gmail.com>
2025-12-29 19:23:26 +0530

    (fix): added frontend url

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://invento-five.vercel.app",
     credentials: true,
     optionsSuccessStatus: 200,
   })
